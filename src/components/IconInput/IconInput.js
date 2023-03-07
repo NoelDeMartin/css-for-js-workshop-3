20f918702d9902ad8a7debda7f8bbb9bdcfdc6dd
Noel De Martin <noel@noeldemartin.com>
2023-03-07 18:46:51 +0100

    IconInput solution

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -11,91 +11,66 @@ const IconInput = ({
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
   const variant = Variants[size];
 
   return <Wrapper style={{
     '--width': `${width}px`,
-    '--height': `${variant.height}px`,
-    '--font-size': `${variant.fontSize}px`,
-    '--line-height': `${variant.lineHeight}px`,
-    '--padding-start': `${variant.paddingStart}px`,
+    '--height': `${variant.height}rem`,
+    '--font-size': `${variant.fontSize}rem`,
     '--underline-height': `${variant.underlineHeight}px`,
   }}>
     <VisuallyHidden>{label}</VisuallyHidden>
     <InputIcon id={icon} size={variant.iconSize} />
-    <Input type="text" placeholder={placeholder} />
-    <Underline />
+    <Input type="text" {...delegated} />
   </Wrapper>
 };
 
 const Variants = {
   small: {
-    height: 24,
+    height: 24 / 16,
     iconSize: 16,
-    fontSize: 14,
-    lineHeight: 16,
-    paddingStart: 24,
+    fontSize: 14 / 16,
     underlineHeight: 1,
   },
   large: {
-    height: 35,
+    height: 36 / 16,
     iconSize: 24,
-    fontSize: 18,
-    lineHeight: 21,
-    paddingStart: 32,
+    fontSize: 18 / 16,
     underlineHeight: 2,
   },
 };
 
 const Wrapper = styled.label`
-  --color: ${COLORS.gray700};
-
+  color: ${COLORS.gray700};
   position: relative;
   display: block;
   width: var(--width);
   height: var(--height);
-  isolation: isolate;
 
   &:hover {
-    --color: ${COLORS.black};
+    color: ${COLORS.black};
   }
 `;
 
 const InputIcon = styled(Icon)`
   position: absolute;
-  margin: auto;
+  margin: auto 0;
   top: 0;
   bottom: 0;
-  color: var(--color);
-  pointer-events: none;
-`;
-
-const Underline = styled.span`
-  position: absolute;
-  bottom: 2px;
-  right: 2px;
-  left: 2px;
-  height: var(--underline-height);
-  background: ${COLORS.black};
-  pointer-events: none;
-  z-index: 1;
 `;
 
 const Input = styled.input`
-  color: var(--color);
+  color: inherit;
   width: 100%;
   height: 100%;
   border: 0;
+  border-bottom: var(--underline-height) solid ${COLORS.black};
   font-weight: 700;
-  padding-inline-start: var(--padding-start);
+  padding-inline-start: var(--height);
   font-size: var(--font-size);
-  line-height: var(--font-size);
-
-  &:hover {
-    color: var(--color);
-  }
+  outline-offset: 2px;
 
   &::placeholder {
     color: ${COLORS.gray500};
